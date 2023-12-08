26e56cbec7670d9fc0da599a75e745e935f83e51
Mykolai Skydan <skmykolai@gmail.com>
2023-12-08 12:32:37 +0000

    add build command

--- src/lib/scrapping/getAllArticles.ts
+++ src/lib/scrapping/getAllArticles.ts
@@ -5,7 +5,7 @@ import { getArticleLinks } from "./services/getArticleLinks";
 import { Article } from "./types";
 import "dotenv/config";
 
-const mainUrl = process.env.POSTS_URL! + "/tags/dota-2"
+const mainUrl = process.env.POSTS_URL! + "/tags/dota-2";
 
 export const getAllArticles = async () => {
   try {
@@ -14,23 +14,31 @@ export const getAllArticles = async () => {
     if (!articleUrls) return [];
     const dbArticles = await getArticles();
     let switcher: boolean = false;
-    const promises = articleUrls.slice(0, 5).map(elem => new Promise(async(resolve, reject) => {
-      setTimeout(() => {reject(null)}, 60000)
-      const found = dbArticles.findIndex((dbElem) => dbElem.url === elem.url);
-      if (found !== -1) {
-        switcher = true;
-      }
-      if (switcher) resolve(null);
-      const article = await getArticleData({
-        url: elem.url,
-        mainPicture: elem.mainPicture,
-      });
-      if (!article) resolve(null);
+    const promises = articleUrls.slice(0, 5).map(
+      (elem) =>
+        new Promise(async (resolve, reject) => {
+          setTimeout(() => {
+            reject(null);
+          }, 60000);
+          const found = dbArticles.findIndex(
+            (dbElem) => dbElem.url === elem.url
+          );
+          if (found !== -1) {
+            switcher = true;
+          }
+          if (switcher) resolve(null);
+          const article = await getArticleData({
+            url: elem.url,
+            mainPicture: elem.mainPicture,
+          });
+          if (!article) resolve(null);
 
-      resolve(article);
-    }))
+          resolve(article);
+        })
+    );
     const values = await Promise.all(promises);
     const filtered = values.filter(
+      //@ts-ignore
       (elem) => elem != null && elem?.content && elem.title && elem.url
     );
     await addArticles(filtered as Article[]);
